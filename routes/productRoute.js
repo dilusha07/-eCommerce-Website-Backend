56b02b0e25bde9fb1f242bc4fa104a9d9ab659e4
dilusha07 <dilushasandaruwani07@gmail.com>
2022-09-11 02:41:11 +0530

    Get list of products

---
+++ routes/productRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import Product from "../models/productModel";
+
+const router = express.Router();
+
+//Get List of Products
+router.get("/", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
+
+export default router;
