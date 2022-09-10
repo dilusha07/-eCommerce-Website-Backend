3e7706f6845b85705d585d9d421a4ba8bc0389e4
dilusha07 <dilushasandaruwani07@gmail.com>
2022-09-11 02:42:48 +0530

    Create new product

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -9,4 +9,22 @@ router.get("/", async (req, res) => {
   res.send(products);
 });
 
+//Create new Product
+router.post("/", async (req, res) => {
+  const product = new Product({
+    sku: req.body.sku,
+    image: req.body.image,
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+  });
+  const newProduct = await product.save();
+  if (newProduct) {
+    return res
+      .status(201)
+      .send({ message: "New Product Created", data: newProduct });
+  }
+  return res.status(500).send({ message: "Error in Creating Product." });
+});
+
 export default router;
