62ccb5a34db4e5c30ef322dcb0d43cef662a2e6e
dilusha07 <dilushasandaruwani07@gmail.com>
2022-09-10 21:08:15 +0530

    Get list of products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,6 +5,12 @@ import Product from "../models/productModel.js";
 
 const productRouter = express.Router();
 
+//Send list of products to frontend
+productRouter.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
+
 //Create sample products
 productRouter.get(
   "/seed",
