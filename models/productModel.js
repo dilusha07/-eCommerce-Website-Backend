378c42f0e295c8141c15e17e1b16778cf201575f
dilusha07 <dilushasandaruwani07@gmail.com>
2022-09-10 19:44:23 +0530

    Create Product model

---
+++ models/productModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  sku: { type: String, required: true, unique: true },
+  image: { type: String, required: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
