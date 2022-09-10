06130cab1318aa6e756efe0150a9d9bafe06211e
dilusha07 <dilushasandaruwani07@gmail.com>
2022-09-10 15:01:45 +0530

    Connect to MondoDB database

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect;
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
+const databaseURL =
+  "mongodb+srv://dilusha07:d1234s123@cluster0.65hdtzy.mongodb.net/ecommerce?retryWrites=true&w=majority";
+mongoose
+  .connect(databaseURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log("Connected to DB");
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.get("/", (req, res) => {
   res.send("Server is ready...");
-});
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
 });
