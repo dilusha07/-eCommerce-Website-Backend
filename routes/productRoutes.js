ec41447aeded82bf60ef8c0f411c5c73a12ba734
dilusha07 <dilushasandaruwani07@gmail.com>
2022-09-10 21:10:50 +0530

    Get product details

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -21,4 +21,14 @@ productRouter.get(
   })
 );
 
+//Api for product details to frontend
+productRouter.get("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+
 export default productRouter;
